67abf548d1e0f05aadee4510a6d2ce512709b34a
Sumitkumar988 <sumitkumarasus0001@gmail.com>
2026-09-25 20:01:02 +0530

    Add program to swap two numbers

---
+++ UNIT1/SWAPPIN .c
@@ -0,0 +1,22 @@
+//THIS PROGRAM SHOWS SWAPPING OF 2 NUMBERS:-
+
+
+#include <stdio.h>
+int main()
+{
+int num1,num2,temp;
+printf("Enter num1:");
+scanf("%d", &num1);
+printf("Enter num2:");
+scanf("%d", &num2);
+
+temp = num1;
+num1 = num2;
+num2 = temp;
+
+printf("After swapping: num1 = %d, num2= %d", num1, num2);
+return 0;
+
+}
+
+
